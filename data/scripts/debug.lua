3a1c84e95fd3b37415753decb9b2c9d28c6a6b7e
Subsuperhuman <joel@barbarianquest.com>
2016-12-04 22:04:14 +0000

    added door logic to main.lua

--- data/scripts/debug.lua
+++ data/scripts/debug.lua
@@ -9,16 +9,4 @@ if(input:key_pressed("test")) then
 	p1t:move(0,1)
 end
 
-events = get_global_events()
-
-for k,ev in pairs(events) do 
-	if(string.match(ev.script,"collision")) then
-		if(string.match(ev.script,"player_1")) then
-			if(string.match(ev.script,"house_1")) then
-				change_level("qa_space")
-			end
-		end
-	end
-end
-
 input:set_key_input("test","1")
--- data/scripts/main.lua
+++ data/scripts/main.lua
@@ -1,1 +1,22 @@
-print("you're in main 3")
+events = get_global_events()
+
+
+for k,ev in pairs(events) do 
+	if(string.match(ev.script,"collision")) then
+		if(string.match(ev.script,"player_1") and string.match(ev.script,"door")) then
+			--get player in current level
+			if current_level() == "butterfly_demo" then
+				change_level("qa_space")
+				p1t = get_obj_transform("player_1")
+				p1t.position.x = 100
+				p1t.position.y = 140
+
+			elseif current_level() == "qa_space" then
+				change_level("butterfly_demo")
+				p1t = get_obj_transform("player_1")
+				p1t.position.x = 505
+				p1t.position.y = 280
+			end
+		end
+	end
+end
