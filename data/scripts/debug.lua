eb2e24e66ca5892ff859b564871ce481c34b71d1
Subsuperhuman <joel@barbarianquest.com>
2016-12-08 12:12:07 +0000

    added an example object removal script in debug

--- data/scripts/debug.lua
+++ data/scripts/debug.lua
@@ -5,8 +5,7 @@ input = get_global_input()
 
 keysdown = input:get_keys_down()
 if(input:key_pressed("test")) then
-	p1t = get_transform(1)
-	p1t:move(0,1)
+	remove_object("house_1")
 end
 
 input:set_key_input("test","1")
