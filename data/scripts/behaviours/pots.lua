921dbceded33a5a58041d1558f17c3ffee25e813
Subsuperhuman <joel@barbarianquest.com>
2017-01-07 17:58:01 +0000

    fixed pot scripts to work with self arguments

--- data/scripts/behaviours/pots.lua
+++ data/scripts/behaviours/pots.lua
@@ -1,22 +1,23 @@
 Pot = {}
 
 --called on spawn
-function Pot:start(self)
+function Pot:start(object)
 end
 
 --called upon activation
-function Pot:wake(self)
+function Pot:wake(object)
 end
 
 --called every loop
-function Pot:update(self)
-	print(self.name)
+function Pot:update(object)
+	-- t = get_obj_transform(object)
+	-- t:move(0,0.1)
 end
 
 --called when the object is made inactive
-function  Pot:sleep(self)
+function  Pot:sleep(object)
 end
 
 -- called upon deactivation
-function Pot:finish(self)
+function Pot:finish(object)
 end
